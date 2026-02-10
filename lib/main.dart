655d514753aff927aec614e63487bcdf9e73134b
Zyad mohammed <ziadmhmdaly@gmail.com>
2026-02-10 23:39:33 +0200

    delete const

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: const NotesView(),
+      home:  NotesView(),
     );
   }
 }
