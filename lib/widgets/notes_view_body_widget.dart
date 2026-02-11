1f11f31441fb1e6119fe331eea5bdf182afc9d3c
Zyad mohammed <ziadmhmdaly@gmail.com>
2026-02-11 14:26:07 +0200

    implement floatingpoint

--- lib/widgets/notes_view_body_widget.dart
+++ lib/widgets/notes_view_body_widget.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar_widget.dart';
-import 'package:notes_app/widgets/custom_notes_item.dart';
-import 'package:notes_app/widgets/list_view_notes_item.dart';
+import 'package:notes_app/widgets/notes_list_view_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(18.0),
+    return const Padding(
+      padding: EdgeInsets.all(18.0),
       child: Column(
-        children: [SizedBox(height: 30), CustomAppBar(), ListViewNotesItem()],
+        children: [SizedBox(height: 20), CustomAppBar(), NotesListViewItem()],
       ),
     );
   }
