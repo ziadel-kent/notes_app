a9fa0fe350d7dccd5db65f934870f5df2453f1e7
Zyad mohammed <ziadmhmdaly@gmail.com>
2026-02-13 23:42:42 +0200

    delete note

--- lib/views/widgets/custom_notes_item.dart
+++ lib/views/widgets/custom_notes_item.dart
@@ -4,15 +4,20 @@ import 'package:intl/intl.dart';
 import 'package:notes_app/model/notes_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
-class CustomNotesItem extends StatelessWidget {
+class CustomNotesItem extends StatefulWidget {
   const CustomNotesItem({super.key, required this.notes});
   final NotesModel notes;
 
   @override
+  State<CustomNotesItem> createState() => _CustomNotesItemState();
+}
+
+class _CustomNotesItemState extends State<CustomNotesItem> {
+  @override
   Widget build(BuildContext context) {
-    final dt = DateTime.tryParse(notes.date);
+    final dt = DateTime.tryParse(widget.notes.date);
     final formattedDate =
-        dt != null ? DateFormat('MMMM dd, yyyy').format(dt) : notes.date;
+        dt != null ? DateFormat('MMMM dd, yyyy').format(dt) : widget.notes.date;
     return GestureDetector(
       onTap: () {
         Navigator.push(
@@ -29,7 +34,7 @@ class CustomNotesItem extends StatelessWidget {
           padding: EdgeInsets.only(left: 20, bottom: 20, top: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
-            color: Color(notes.color),
+            color: Color(widget.notes.color),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
@@ -37,18 +42,21 @@ class CustomNotesItem extends StatelessWidget {
               ListTile(
                 contentPadding: EdgeInsets.all(0),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    widget.notes.delete();
+                    setstate() {}
+                  },
                   icon: Icon(Icons.delete, color: Colors.black, size: 32),
                 ),
                 title: Text(
-                  notes.title,
+                  widget.notes.title,
                   style: TextStyle(color: Colors.black, fontSize: 26),
                 ),
 
                 subtitle: Padding(
                   padding: EdgeInsets.only(top: 20),
                   child: Text(
-                    notes.subTitle,
+                    widget.notes.subTitle,
                     style: TextStyle(
                       color: Colors.black.withOpacity(.6),
                       fontSize: 20,
